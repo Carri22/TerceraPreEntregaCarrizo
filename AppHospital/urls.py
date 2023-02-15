2a7b2f0800ebe92f37a7d0f2b3121d4186d97354
Carri22 <108766795+Carri22@users.noreply.github.com>
2023-02-14 22:11:15 -0300

    Medicos y pacientes actualizados

--- AppHospital/urls.py
+++ AppHospital/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('medico-formulario/',medico_formulario,name="medico-formulario"),
     path('busqueda-especialidad/',busqueda_especialidad,name="busqueda-especialidad"),
     path('buscar/',buscar,name="buscar"),
-    # path('leer-medicos/',leer_medicos,name="leer-medicos"),
+    path('paciente-formulario/',peciente_formulario, name="paciente-formulario"),
+    # path('paciente/',paciente,name="paciente-formulario"),
 
 ]
--- AppHospital/views.py
+++ AppHospital/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import redirect, render
 from AppHospital.models import Medico,Paciente,MedicoInterno
-from .forms import MedicoFormulario
+from .forms import MedicoFormulario,PacienteFormulario
 from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
+    #Retorna a la vista padre-inicio
     return render(request,'AppHospital/padre.html')
 
+#FUNCIONES DE MEDICO
 def medico(request):
+    #retorna a la vista medico
     return render(request,'AppHospital/medico.html')
 
 def medico_formulario(request):
-    
+
+    #almacena en la variable todos los medicos registrados 
     mis_medicos = Medico.objects.all()
 
     #Aqui recibiremos toda la informacion enviada mediante el formulario
@@ -21,13 +25,16 @@ def medico_formulario(request):
         #Validamos que los datos correspondan a los esperados
         if mi_formulario.is_valid():
             informacion = mi_formulario.cleaned_data
+            #Crea un objeto medico con la informacion
             medico = Medico(
                 nombre = informacion['nombre'],
                 apellido=informacion['apellido'],
                 especialidad=informacion['especialidad']
                 )
+            #Guarda el objeto en la base de datos 
             medico.save()
-            return redirect('inicio')
+            #vuelve a redireccionar a la vista del formulario para poder seguir registrando medicos
+            return redirect('medico-formulario')
 
     else:
         #Inicializamos un formulario vacio para construir el HTML
@@ -48,3 +55,37 @@ def buscar(request):
         respuesta = f'No se encontro la especialidad:{request.GET["especialidad"]}'
           
     return HttpResponse(respuesta)
+
+#FUNCIONES DE PACIENTE
+def peciente(request):
+    #retorna a la vista paciente
+    return render(request,'AppHospital/peciente.html')
+
+def peciente_formulario(request):
+
+    #almacena en la variable todos los pacientes registrados 
+    mis_pecientes = Paciente.objects.all()
+
+    #Aqui recibiremos toda la informacion enviada mediante el formulario
+    if request.method == "POST":
+        mi_formulario = PacienteFormulario(request.POST)
+
+        #Validamos que los datos correspondan a los esperados
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            #Crea el objeto paciente con la informacion
+            paciente = Paciente(
+                nombre = informacion['nombre'],
+                apellido=informacion['apellido'],
+                email=informacion['email']
+                )
+            #guarda el objeto en la base de datos
+            paciente.save()
+            #vuelve a redireccionar a la vista del formulario para poder seguir registrando medicos
+            return redirect('paciente-formulario')
+
+    else:
+        #Inicializamos un formulario vacio para construir el HTML
+        mi_formulario = PacienteFormulario()
+    return render(request, 'AppHospital/paciente-formulario.html',{'formulario_paciente': mi_formulario, 'mis_paciente':mis_pecientes})
+
